0eeb07e54f6cc17f1fdec83f1590547c7f983889
Dr-42 <royspandan1@gmail.com>
2025-06-28 11:37:36 +0530

    Library reload hopeful final fix

--- src/api_servers.rs
+++ src/api_servers.rs
@@ -5,8 +5,10 @@ use axum::{
 };
 use hyper::{header, StatusCode};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::HashMap;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Meta {
     #[serde(skip_serializing_if = "Option::is_none")]
     adult: Option<bool>,
@@ -31,18 +33,23 @@ pub struct Meta {
     #[serde(skip_serializing_if = "Option::is_none")]
     title: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     video: Option<bool>,
     vote_average: f32,
     vote_count: u32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct MetaData {
     series: Vec<Meta>,
     movies: Vec<Meta>,
+    #[serde(rename = "fileDatabase")]
+    file_database: HashMap<String, Value>,
 }
 
 pub async fn add_media(media: Json<MetaData>) -> impl IntoResponse {
+    println!("{:?}", media);
     let data_path = directories::ProjectDirs::from("com", "dr42", "nexus").unwrap();
     let data_dir = data_path.data_dir();
     if !data_dir.exists() {
@@ -62,6 +69,14 @@ pub async fn get_media() -> impl IntoResponse {
     let data_path = directories::ProjectDirs::from("com", "dr42", "nexus").unwrap();
     let data_dir = data_path.data_dir();
     let metadata_file = data_dir.join("meta.json");
+
+    if !metadata_file.exists() {
+        return Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from("Metadata file not found."))
+            .unwrap();
+    }
+
     let json_data = std::fs::read_to_string(metadata_file).unwrap();
     Response::builder()
         .status(StatusCode::OK)
